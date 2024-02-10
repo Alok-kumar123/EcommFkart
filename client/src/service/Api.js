9946b52d1bd4c7e95882ed2c9cf60fa24d040e52
Alok-kumar123 <124078764+Alok-kumar123@users.noreply.github.com>
2024-02-10 17:28:54 +0530

    Update url in Api.js

--- client/src/service/Api.js
+++ client/src/service/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
  
 
-const URL="http://localhost:8000";
+const URL="https://ecomm-fkart-server.vercel.app";
 export const authenticateSignup = async(data) => {
     try {
        return await axios.post(`${URL}/signup`,data)

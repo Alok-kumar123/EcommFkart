ec645adec0a41486ebefa1e20bdc6108d373ca0f
Alok-kumar123 <124078764+Alok-kumar123@users.noreply.github.com>
2024-02-10 17:12:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ const PASSWORD=process.env.DB_PASSWORD;
 
 const URL= process.env.MONGODB_URI||`mongodb://${USERNAME}:${PASSWORD}@ac-jpdynfb-shard-00-00.xlvron0.mongodb.net:27017,ac-jpdynfb-shard-00-01.xlvron0.mongodb.net:27017,ac-jpdynfb-shard-00-02.xlvron0.mongodb.net:27017/?ssl=true&replicaSet=atlas-n3a1ua-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-app.use(cors());  //Cross origin resource sharing
+app.use(cors(
+ {
+  origin:["https://ecomm-fkart-server.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+ }
+));  //Cross origin resource sharing
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/',Router);

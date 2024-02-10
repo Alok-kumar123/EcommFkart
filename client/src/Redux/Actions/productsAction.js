7458d02f4e1529edc858279b97ee237e3672bb11
Alok-kumar123 <124078764+Alok-kumar123@users.noreply.github.com>
2024-02-10 17:38:02 +0530

    Update  url in productsAction.js

--- client/src/Redux/Actions/productsAction.js
+++ client/src/Redux/Actions/productsAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as actionTypes from '../constants/productsConstants';
 
-const URL='http://localhost:8000';
+const URL='https://ecomm-fkart-server.vercel.app';
 
 export const getProducts=()=>async(dispatch)=>{
     try {
